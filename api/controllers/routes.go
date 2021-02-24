869436e46ce9e8c1957358b74b232b759658122b
iamsyahidi <ilhamsyahidi66@gmail.com>
2021-02-24 07:34:04 +0700

    Fix handle delete user

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -22,6 +22,6 @@ func (s *Server) initializeRoutes() {
 	s.Router.HandleFunc("/posts", middlewares.SetMiddlewareJSON(s.GetPosts)).Methods("GET")
 	s.Router.HandleFunc("/posts/{id}", middlewares.SetMiddlewareJSON(s.GetPost)).Methods("GET")
 	s.Router.HandleFunc("/posts/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.UpdatePost))).Methods("PUT")
-	s.Router.HandleFunc("posts/{id}", middlewares.SetMiddlewareAuthentication(s.DeletePost)).Methods("DELETE")
+	s.Router.HandleFunc("/posts/{id}", middlewares.SetMiddlewareAuthentication(s.DeletePost)).Methods("DELETE")
 
 }
