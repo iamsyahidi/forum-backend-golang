4d9171cf990470cad2af2b39425903fabde773b7
iamsyahidi <ilhamsyahidi66@gmail.com>
2021-02-24 07:33:35 +0700

    Fixing dummy data

--- api/seed/seeder.go
+++ api/seed/seeder.go
@@ -9,47 +9,54 @@ import (
 
 var users = []models.User{
 	models.User{
-		Nickname: "Steven victor",
-		Email: "steven@gmail.com",
-		Password: "password",
+		Nickname: "Ilham Syahidi",
+		Email:    "iamsyahidi@gmail.com",
+		Password: "p455word",
 	},
 	models.User{
-		Nickname: "Martin Luther",
-		Email: "luther@gmail.com",
-		Password: "password",
+		Nickname: "Dummy",
+		Email:    "dummy@gmail.com",
+		Password: "p455word",
 	},
 }
 
 var posts = []models.Post{
 	models.Post{
-		Title: "Title 1",
-		Content: "Hello world 1",
+		Title:   "Title 3",
+		Content: "Hello world 3",
 	},
 	models.Post{
-		Title: "Title 2",
-		Content: "Hello world 2",
+		Title:   "Title 4",
+		Content: "Hello world 4",
 	},
 }
 
-func Load(db *gorm.DB)  {
+func Load(db *gorm.DB) {
+
 	err := db.Debug().DropTableIfExists(&models.Post{}, &models.User{}).Error
 	if err != nil {
-		log.Fatalf("cannot drop table : %v", err)
+		log.Fatalf("cannot drop table: %v", err)
 	}
-	err = db.Debug().Model(&models.Post{}).AddForeignKey("author_id", "Users(id)", "cascade", "cascade").Error
+	err = db.Debug().AutoMigrate(&models.User{}, &models.Post{}).Error
 	if err != nil {
-		log.Fatalf("attaching foreign key error : %v", err)
+		log.Fatalf("cannot migrate table: %v", err)
 	}
+
+	err = db.Debug().Model(&models.Post{}).AddForeignKey("author_id", "users(id)", "cascade", "cascade").Error
+	if err != nil {
+		log.Fatalf("attaching foreign key error: %v", err)
+	}
+
 	for i, _ := range users {
 		err = db.Debug().Model(&models.User{}).Create(&users[i]).Error
 		if err != nil {
-			log.Fatalf("cannot seed users table : %v", err)
+			log.Fatalf("cannot seed users table: %v", err)
 		}
 		posts[i].AuthorID = users[i].ID
+
 		err = db.Debug().Model(&models.Post{}).Create(&posts[i]).Error
 		if err != nil {
-			log.Fatalf("cannot seed posts table : %v", err)
+			log.Fatalf("cannot seed posts table: %v", err)
 		}
 	}
-	
 }
